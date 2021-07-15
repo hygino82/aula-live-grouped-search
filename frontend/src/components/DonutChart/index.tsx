33e76db6cbc5b3ba766f861f1850b169232ee8e2
Adroaldo <hygino@msn.com>
2021-07-15 09:37:38 -0300

    Final

---
+++ frontend/src/components/DonutChart/index.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
+import { SaleByStore } from 'types/sale';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
+
+const DonutChart = () => {
+
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/sales/by-store`)
+            .then(response => {
+                const data = response.data as SaleByStore[];
+                const myLabels = data.map(x => x.storeName);
+                const mySeries = data.map(x => x.sum);
+    
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
+    
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
+    );
+}
+
+export default DonutChart; 
